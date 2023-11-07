8f8fb4f918916a4b95c0769a7e0a4907eba9f51d
Vernon Coffey <59817868+vccoffey@users.noreply.github.com>
2023-11-06 16:27:06 -0800

    adjust directory structure from last commit

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   customSyntax: 'postcss-scss',
   extends: ['stylelint-prettier/recommended'],
-  plugins: ['./stylelint_plugins/nitid_use_stylekit'],
+  plugins: ['./stylelint_plugin_nitid_use_stylekit'],
   rules: {
     'nitid/use-stylekit': [['font-size', 'font-family'], true],
   },
