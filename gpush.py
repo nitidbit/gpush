12a2153e34ae8df3329aef2c7bb3f3101d82e12a
Winston <winston@nitidbit.com>
2024-01-16 14:56:33 -0800

    Rudimentary mechanism for default gpush configration

--- gpush.py
+++ gpush.py
@@ -3,10 +3,15 @@ import yaml
 import subprocess
 
 def find_and_parse_config():
+    with open('gpushrc_default.yml') as f:
+        config = yaml.safe_load(f)
+
     with open('gpushrc.yml') as f:
-        data = yaml.safe_load(f)
-        print('!!! find_and_parse_config data=', repr(data))
-        return data
+        user_config = yaml.safe_load(f)
+        config.update(user_config)
+
+    print('!!! find_and_parse_config() config=', repr(config))
+    return config
 
 def run_commands(list_of_commands):
     for cmd in list_of_commands:
@@ -15,6 +20,8 @@ def run_commands(list_of_commands):
 
 if __name__ == '__main__':
     yml = find_and_parse_config()
+
+    root_dir = yml
 
     run_commands(yml['pre_run'])
     run_commands(yml['post_run'])
