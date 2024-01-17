6ccbce3d95e74661ed5b88d39b8b7f12c6f8c50c
Paola Castro <paolacastropaolacastro@gmail.com>
2024-01-17 14:34:19 -0800

    handling if clause

--- command.py
+++ command.py
@@ -1,19 +1,29 @@
 import subprocess
 import os
 
+def check_type(line_intro, value, expected_type, explanation):
+    if not isinstance(value, expected_type):
+        raise RuntimeError(f'Problem with {line_intro}.  {repr(value)} needs to be of type {repr(expected_type)}.  {explanation}')
+
 def run(list_of_commands):
     for cmd in list_of_commands:
-        if not isinstance(cmd, dict):
-            raise RuntimeError(f'Hi! your command needs to be hash, but it was: {repr(cmd)}')
+        check_type('Command', cmd, dict, 'Commands need to be a hash with at least a "shell:" line.')
 
         if 'shell' not in cmd:
             raise RuntimeError(f'Hi! you need to have a field "shell" in your command: {repr(cmd)}')
 
+        if 'if' in cmd:
+            ifcommand = cmd['if']
+            check_type('"if" clause', ifcommand, str, 'If you have an "if:" clause, it must have a string which will be run in the shell.')
+            result = subprocess.run(ifcommand, shell=True)
+            if result.returncode != 0:
+                print(f'We are skipping {repr(cmd["shell"])} because if clause returned {result.returncode}. Expected 0.')
+                return
+
         env = None
         if 'env' in cmd:
             env = cmd['env']
-            if not isinstance(env, dict):
-                raise RuntimeError(f'Hi! your enviroment variables need to be a dictionary, but instead we found: {repr(env)}')
+            check_type('"env" clause', env, dict, 'An optional "env:" section must have a hash of variable names and values.')
             env = dict(**env, **os.environ)
 
         shell = cmd['shell']
