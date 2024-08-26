172ab59544837515e700208dfa451c59d33aecec
Vernon Coffey <59817868+vccoffey@users.noreply.github.com>
2024-08-26 16:44:12 -0700

    Update gpush.rb

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -6,10 +6,22 @@ class Gpush < Formula
       revision: "be65e6afa9a2cab1998dd3fa04b844bac1911200"
   license "MIT"
 
+  depends_on "python@3.12"
+
   def install
+    # Install the Ruby scripts to the bin directory
     bin.install "src/ruby/gpush_get_specs.rb" => "gpush_get_specs"
     bin.install "src/ruby/gpush_run_if_any.rb" => "gpush_run_if_any"
     bin.install "src/ruby/gpush_options_parser.rb" => "gpush_options_parser"
     bin.install "src/ruby/gpush_changed_files.rb" => "gpush_changed_files"
+
+    # Install the Python package using pip
+    system "python3", "-m", "pip", "install", "git+https://github.com/nitidbit/gpush.git@release/v2-hackathon"
+
+    # Create a wrapper script for the `gpush` command
+    (bin/"gpush").write <<~EOS
+      #!/bin/bash
+      exec python3 -m gpush "$@"
+    EOS
   end
 end
