0187c07a66102602ee8efdc19c26498cadc356ea
Vernon Coffey <59817868+vccoffey@users.noreply.github.com>
2024-08-26 16:55:27 -0700

    update python to have an executable

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -8,7 +8,6 @@ class Gpush < Formula
   version "0.0.0"
 
   depends_on "python@3.12"
-  depends_on "pipx"  # Add pipx as a dependency if you're using it
 
   def install
     # Install the Ruby scripts to the bin directory
@@ -17,11 +16,13 @@ class Gpush < Formula
     bin.install "src/ruby/gpush_options_parser.rb" => "gpush_options_parser"
     bin.install "src/ruby/gpush_changed_files.rb" => "gpush_changed_files"
 
-  # system "pipx", "install", "git+https://github.com/nitidbit/gpush.git@release/v2-hackathon"
+    # Install the Python package using pip in user space
+    system "python3", "-m", "pip", "install", "--user", "git+https://github.com/nitidbit/gpush.git@release/v2-hackathon"
 
-  # # Ensure the installed Python scripts are linked
-  #   python_user_base = `python3 -m site --user-base`.chomp
-  #   python_bin_dir = "#{python_user_base}/bin"
-  #   bin.install_symlink "#{python_bin_dir}/gpush" => "gpush"
+    # Create a wrapper script for the `gpush` command
+    (bin/"gpush").write <<~EOS
+      #!/bin/bash
+      exec python3 -m gpush "$@"
+    EOS
   end
 end
