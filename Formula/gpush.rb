ee8128684a7e653ba700368083df26d77515c17c
Vernon Coffey <59817868+vccoffey@users.noreply.github.com>
2024-08-26 17:17:37 -0700

    Update gpush.rb

--- Formula/gpush.rb
+++ Formula/gpush.rb
@@ -8,21 +8,33 @@ class Gpush < Formula
   version "0.0.0"
 
   depends_on "python@3.12"
-  depends_on "pipx"  # Add pipx as a dependency if you're using it
+  depends_on "pipx"
 
   def install
+    # Logging the start of the installation process
+    ohai "Starting installation of gpush"
+
     # Install the Ruby scripts to the bin directory
+    ohai "Installing Ruby scripts to the bin directory"
     bin.install "src/ruby/gpush_get_specs.rb" => "gpush_get_specs"
     bin.install "src/ruby/gpush_run_if_any.rb" => "gpush_run_if_any"
     bin.install "src/ruby/gpush_options_parser.rb" => "gpush_options_parser"
     bin.install "src/ruby/gpush_changed_files.rb" => "gpush_changed_files"
 
-    # Install the Python package using pip in the user space
-    system "python3", "-m", "pip", "install", "--user", "git+https://github.com/nitidbit/gpush/commit/894b5598253639abbbce31bd25ec39ff6a5a6b0e"
+    # Install the Python package using pipx
+    ohai "Installing the Python package using pipx"
+    system "pipx", "install", "git+https://github.com/nitidbit/gpush.git@894b5598253639abbbce31bd25ec39ff6a5a6b0e"
 
-    # Ensure the installed Python scripts are linked
-    python_user_base = `python3 -m site --user-base`.chomp
-    python_bin_dir = "#{python_user_base}/bin"
-    bin.install_symlink "#{python_bin_dir}/gpush" => "gpush"
+    # Assuming pipx installed the gpush binary in the standard location
+    pipx_bin_path = "#{ENV['HOME']}/.local/pipx/venvs/gpush/bin/gpush"
+    if File.exist?(pipx_bin_path)
+      ohai "Found gpush binary at #{pipx_bin_path}, creating symlink in Homebrew bin directory"
+      bin.install_symlink pipx_bin_path => "gpush"
+    else
+      odie "Failed to locate gpush binary installed by pipx at #{pipx_bin_path}"
+    end
+
+    # Confirming the installation
+    ohai "gpush installation completed"
   end
 end
