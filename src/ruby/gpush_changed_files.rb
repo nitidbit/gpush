dc9abd5c2bc1813411e540d865a5bef83d24676a
Vernon Coffey <59817868+vccoffey@users.noreply.github.com>
2025-07-25 12:21:01 -0700

    Update gpush_changed_files.rb

--- src/ruby/gpush_changed_files.rb
+++ src/ruby/gpush_changed_files.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "English"
+require_relative "gpush" # sets version
 require_relative "git_helper"
 require_relative "gpush_options_parser"
 require_relative "exit_helper"
