2135c181d2d959d3f80fa41783e944cf61c41939
Vernon Coffey <59817868+vccoffey@users.noreply.github.com>
2025-07-25 11:56:00 -0700

    Update gpush_fix.rb

--- src/ruby/gpush_fix.rb
+++ src/ruby/gpush_fix.rb
@@ -5,32 +5,24 @@ require_relative "gpush_error"
 
 module GpushFix
   def self.go(args:, options:)
-    config_file = options.delete(:config_file)
-
-    if options.any?
-      puts "Unexpected option(s): #{options.keys.join(", ")}"
-      puts "Run 'gpush --help' for usage information."
-      ExitHelper.exit(1)
-    elsif args.any?
+    if args.any?
       puts "Unexpected argument(s): #{args.join(", ")}"
       puts "gpush fix does not accept any arguments."
       puts "Run 'gpush --help' for usage information."
       ExitHelper.exit(1)
     end
 
-    config = ConfigHelper.parse_config(config_file)
-
-    if config["fix"].nil?
+    if options[:fix].nil?
       puts "No fix section found in config file"
       ExitHelper.exit(1)
     end
 
-    if config["fix"].empty?
+    if options[:fix].empty?
       puts "Fix section is empty"
       ExitHelper.exit(1)
     end
 
-    config["fix"].each do |cmd_dict|
+    options[:fix].each do |cmd_dict|
       command = Command.new(cmd_dict, verbose: true, prefix_output: false)
       command.run
     end
